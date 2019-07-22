7a35adf0790b6196ed5cd08c3dc9a1774b4dc8d7
Betty van Aken <bettyvanaken@gmail.com>
2019-07-22 12:34:16 +0200

    implement db re-connection

--- src/db.py
+++ src/db.py
@@ -14,47 +14,65 @@ cursor = nohate_db.cursor()
 logger = logging.getLogger(__name__)
 
 
-def duplicate_prev_id(text, lang):
+def reconnect(retry_function, *args):
+    nohate_db.reconnect(attempts=5, delay=2)
+    logging.info("Reconnected to DB.")
+
+    if nohate_db.is_connected():
+        return retry_function(*args)
+
+
+def fetch_last_entry():
     text_from_id_command = "SELECT id, text, lang FROM comments WHERE id = (SELECT MAX(id) FROM comments);"
     cursor.execute(text_from_id_command)
 
-    result = cursor.fetchone()
+    return cursor.fetchone()
 
-    if result is not None:
 
-        prev_text = result[1]
-        prev_lang = result[2]
+def duplicate_prev_id(text, lang):
+    last_entry = fetch_last_entry()
 
-        logging.debug("text_from_id_command: " + str(result))
+    if last_entry is not None:
+
+        prev_text = last_entry[1]
+        prev_lang = last_entry[2]
 
         if text == prev_text and lang == prev_lang:
-            return result[0]
+            return last_entry[0]
 
     return None
 
 
 def insert_comment(text, date, lang, label=-1):
-    duplicate_id = duplicate_prev_id(text, lang)
+    try:
+        duplicate_id = duplicate_prev_id(text, lang)
 
-    if duplicate_id is None:
+        if duplicate_id is None:
 
-        insert_command = "INSERT INTO comments(text, date, label, lang) VALUES(%s, %s, %s, %s);"
-        row_input = (text, date, label, lang)
-        cursor.execute(insert_command, row_input)
-        nohate_db.commit()
+            insert_command = "INSERT INTO comments(text, date, label, lang) VALUES(%s, %s, %s, %s);"
+            row_input = (text, date, label, lang)
+            cursor.execute(insert_command, row_input)
+            nohate_db.commit()
 
-        return cursor.lastrowid
+            return cursor.lastrowid
 
-    else:
-        logging.debug("Double entry, comment not inserted.")
+        else:
+            logging.debug("Double entry, comment not inserted.")
 
-        return duplicate_id
+            return duplicate_id
+
+    except mysql.connector.errors.DatabaseError:
+        return reconnect(insert_comment, text, date, lang, label)
 
 
 def update_comment(comment_id, label):
-    update_command = "UPDATE comments SET label = %s WHERE id = %s;"
-    row_update = (label, comment_id)
-    cursor.execute(update_command, row_update)
-    nohate_db.commit()
+    try:
+        update_command = "UPDATE comments SET label = %s WHERE id = %s;"
+        row_update = (label, comment_id)
+        cursor.execute(update_command, row_update)
+        nohate_db.commit()
 
-    return comment_id
+        return comment_id
+
+    except mysql.connector.errors.DatabaseError:
+        return reconnect(insert_comment, comment_id, label)
